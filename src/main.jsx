3585b27e0f245307dbbaff499951b4561ddcf716
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-11-11 15:53:50 +0200

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
 );
